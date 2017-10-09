60cf4a9b5c2067f4595899ea35f6cc758c2ab175
phisyche <phisyche@gmail.com>
2017-10-09 16:39:20 +0300

    2nd commit

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,3 +2,8 @@
 module.exports.index = function(req, res){
 res.render('index', { title: 'SokoEstate' });
 };
+
+/* GET 'about' page */
+module.exports.about = function(req, res){
+res.render('index', { title: 'About' });
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlProperties = require('../controllers/properties');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlProperties.homelist);
+router.get('/property', ctrlProperties.propertyInfo);
+router.get('/property/new', ctrlProperties.addProperty);
+
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
