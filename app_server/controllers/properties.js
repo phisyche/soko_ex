e6be482478549ab5f37938b923b583061f9c9fb0
phisyche <phisyche@gmail.com>
2017-10-13 18:36:21 +0300

    6th commit

--- app_server/controllers/properties.js
+++ app_server/controllers/properties.js
@@ -1,18 +1,14 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-res.render('index', { title: 'Home' });
+res.render('property_list', { title: 'Home' });
 };
 
 /* GET 'Property info' page */
 module.exports.propertyInfo = function(req, res){
-res.render('index', { title: 'Properties in Kenya' });
+res.render('property_info', { title: 'Properties in Kenya' });
 };
 
 /* GET 'Add review' page */
 module.exports.addProperty = function(req, res){
 res.render('index', { title: 'Add Property' });
-};
-
-module.exports.homelist = function(req, res){
-res.render('property-list', { title: 'Home' });
 };
